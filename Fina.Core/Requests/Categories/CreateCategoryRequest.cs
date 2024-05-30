43c6732af14cd10fd9f007475df58c96e5cbca36
jeferson <jef_shandler@msn.com>
2024-05-30 15:26:12 -0300

    Add project files.

---
+++ Fina.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Categories
+{
+    public class CreateCategoryRequest : Request
+    {
+        [Required(ErrorMessage = "Titulo inválido")]
+        [MaxLength(80, ErrorMessage = "O título deve conter até 80 caracteres!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Descrição inválida")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ Fina.Core/Requests/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace Fina.Core.Requests
+{
+    public abstract class Request
+    {
+        public string UserID { get; set; } = string.Empty;
+    }
+
+    
+}
